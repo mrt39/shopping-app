91357a1d1022deef3d5b928fe922e6097bdf3e58
mrt39 <mertoktar39@gmail.com>
2023-09-05 19:51:19 +0300

    Create seperate component for snackbar

--- src/routes/StorePage.jsx
+++ src/routes/StorePage.jsx
@@ -1,21 +1,10 @@
+import * as React from 'react';
 import ItemCards from '../components/ItemCards';
 import { useEffect, useState, useRef } from 'react'
-import logoImage from '../assets/images/logo.png'
+import logoImage from '../assets/images/logo-v.png'
 import Pagination from '@mui/material/Pagination';
-import { useNavigate } from "react-router-dom";
-
-/* SNACKBAR IMPORTS*/
-/* eslint-disable react/prop-types */
-import * as React from 'react';
-import { Transition } from 'react-transition-group';
-import { styled } from '@mui/system';
-import CheckRoundedIcon from '@mui/icons-material/CheckRounded';
-import CloseIcon from '@mui/icons-material/Close';
-import { Snackbar } from '@mui/base/Snackbar';
-/* SNACKBAR IMPORTS END */
-
-/* GAMEPAGE IMPORT */
 import GamePage from '../components/GamePage.jsx';
+import SnackBarComp from '../components/SnackBar.jsx';
 
 
 
@@ -86,24 +75,6 @@ function ShopPage({handleAddtoCart, apiLink, gamesInCart, changeApiLink}) {
 
 /* SNACKBAR states and functions */
   const [open, setOpen] = useState(false);
-  const [exited, setExited] = useState(true);
-  const nodeRef = useRef(null);
-
-const handleClose = (_, reason) => {
-  if (reason === 'clickaway') {
-    return;
-  }
-  setOpen(false);
-};
-
-
-const handleOnEnter = () => {
-  setExited(false);
-};
-
-const handleOnExited = () => {
-  setExited(true);
-};
 /* SNACKBAR states and functions end */
 
 
@@ -117,9 +88,7 @@ const [gameScreenshots, setGameScreenshots] = useState([]);
 const handleClickOpen = (gameID, gamePrice, gameScreenshots) => {
   //change the gameid state
   setGameID(gameID)
-  console.log(gameID)
   setGameScreenshots(gameScreenshots);
-  console.log(gameScreenshots.gameScreenshots[0].image)
   setGamePrice(gamePrice)
   setGamePageOpen(true);  
 };
@@ -195,48 +164,9 @@ const handleClickOpen = (gameID, gamePrice, gameScreenshots) => {
             <h4 className='storePageNoGamesAlert'>There are no games in the database that match the search!</h4>
           }
 
-      <StyledSnackbar
-        autoHideDuration={5000}
-        open={open}
-        onClose={handleClose}
-        exited={exited}
-      >
-        <Transition
-          timeout={{ enter: 400, exit: 400 }}
-          in={open}
-          appear
-          unmountOnExit
-          onEnter={handleOnEnter}
-          onExited={handleOnExited}
-          nodeRef={nodeRef}
-        >
-          {(status) => (
-            <SnackbarContent
-              style={{
-                transform: positioningStyles[status],
-                transition: 'transform 300ms ease',
-              }}
-              ref={nodeRef}
-            >
-              <CheckRoundedIcon
-                sx={{
-                  color: 'success.main',
-                  flexShrink: 0,
-                  width: '1.25rem',
-                  height: '1.5rem',
-                }}
-              />
-              <div className="snackbar-message">
-                <p className="snackbar-title">Game Added!</p>
-                <p className="snackbar-description">
-                  Game successfully added to the cart.
-                </p>
-              </div>
-              <CloseIcon onClick={handleClose} className="snackbar-close-icon" />
-            </SnackbarContent>
-          )}
-        </Transition>
-      </StyledSnackbar>
+      <SnackBarComp
+      setOpen={setOpen}
+      open={open}/>
       </div>
       : "" 
       } 
@@ -246,103 +176,4 @@ const handleClickOpen = (gameID, gamePrice, gameScreenshots) => {
 }
   
   export default ShopPage
-  
-
-
-
-/* SNACKBAR STYLING */
-  const grey = {
-    50: '#f6f8fa',
-    100: '#eaeef2',
-    200: '#d0d7de',
-    300: '#afb8c1',
-    400: '#8c959f',
-    500: '#6e7781',
-    600: '#57606a',
-    700: '#424a53',
-    800: '#32383f',
-    900: '#24292f',
-  };
-  
-  const blue = {
-    200: '#99CCF3',
-    400: '#3399FF',
-    500: '#007FFF',
-    600: '#0072E5',
-    700: '#0059B2',
-    800: '#004C99',
-    900: '#003A75',
-  };
-  
-  
-  const StyledSnackbar = styled(Snackbar)`
-    position: fixed;
-    z-index: 5500;
-    display: flex;
-    bottom: 50px;
-    right: 16px;
-    max-width: 560px;
-    min-width: 300px;
-  `;
-  
-  const SnackbarContent = styled('div')(
-    ({ theme }) => `
-    display: flex;
-    gap: 8px;
-    overflow: hidden;
-    background-color: ${theme.palette.mode === 'dark' ? grey[900] : grey[50]};
-    border-radius: 8px;
-    border: 1px solid ${theme.palette.mode === 'dark' ? grey[700] : grey[200]};
-    box-shadow: ${
-      theme.palette.mode === 'dark'
-        ? `0 2px 16px rgba(0,0,0, 0.5)`
-        : `0 2px 16px ${grey[200]}`
-    };
-    padding: 0.75rem;
-    color: ${theme.palette.mode === 'dark' ? grey[50] : grey[900]};
-    font-family: IBM Plex Sans, sans-serif;
-    font-weight: 500;
-    text-align: start;
-    position: relative;
-  
-    & .snackbar-message {
-      flex: 1 1 0%;
-      max-width: 100%;
-    }
-  
-    & .snackbar-title {
-      margin: 0;
-      line-height: 1.5rem;
-      margin-right: 0.5rem;
-    }
-  
-    & .snackbar-description {
-      margin: 0;
-      line-height: 1.5rem;
-      font-weight: 400;
-      color: ${theme.palette.mode === 'dark' ? grey[400] : grey[800]};
-    }
-  
-    & .snackbar-close-icon {
-      cursor: pointer;
-      flex-shrink: 0;
-      padding: 2px;
-      border-radius: 4px;
-  
-      &:hover {
-        background: ${theme.palette.mode === 'dark' ? grey[800] : grey[50]};
-      }
-    }
-    `,
-  );
-  
-  const positioningStyles = {
-    entering: 'translateX(0)',
-    entered: 'translateX(0)',
-    exiting: 'translateX(500px)',
-    exited: 'translateX(500px)',
-    unmounted: 'translateX(500px)',
-  };
-  
-  
   
