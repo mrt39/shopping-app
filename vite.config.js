3ca0d9f26424bb7d97b7d2a7076f2480c3995353
mrt39 <mertoktar39@gmail.com>
2023-09-07 15:55:04 +0300

    Change viteconfig

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig(({ command }) => {
   const config = {
     plugins: [react()],
-    base: '/shopping-app/',
+    base: '/',
   }
 
   if (command !== 'serve') {
